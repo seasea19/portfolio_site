c29a3efcf45d0b3d5ef3f031c3d33ad5136f8d8b
Chelsea Alcinord <59550990+seasea19@users.noreply.github.com>
2022-01-13 19:22:49 -0500

    new

--- DADJOKES/script.js
+++ DADJOKES/script.js
@@ -12,7 +12,7 @@ async function generateJoke(){
         },
     }
 
-const res = await fetch('https://icanhazdadjoke.com/',config)
+const res = await fetch('https://icanhazdadjoke.com/',config) 
 
 const data = await res.json()
 
